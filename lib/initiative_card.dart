7c97a1777c60ad7a644ef4633432871da73eb6ca
Aidan Border <planes4u@outlook.com>
2023-02-21 12:31:49 -0500

    Beginning to implement the conditions card

--- lib/initiative_card.dart
+++ lib/initiative_card.dart
@@ -1,12 +1,12 @@
 import 'dart:developer'; //This import gives us access to the log() function. It can be safely removed when all buttons are properly implemented.
 import 'package:flutter/material.dart';
-import 'initiative.dart'; 
+import 'initiative.dart';
 
 class InitiativeCard extends StatefulWidget {
-	// This declaration makes any parameters needed available to instances of the class. The Java equivalent is a constructor method
-	const InitiativeCard({
-		Key? key,
-	}) : super(key: key);
+  // This declaration makes any parameters needed available to instances of the class. The Java equivalent is a constructor method
+  const InitiativeCard({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<InitiativeCard> createState() => _InitiativeCardState();
@@ -14,78 +14,98 @@ class InitiativeCard extends StatefulWidget {
 
 class _InitiativeCardState extends State<InitiativeCard> {
   bool _shouldDisplayConditionsCard = false;
-	/// build()
-	/// Parameters: BuildContext context
-	/// Returns: Widget
-	/// Description: Method to construct the InitiativeCard Widget. Instances of this widget are responsible for displaying and managing the state of the initiative blocks
-	/// Special: Method overrides Widget.build()
-	@override
-	Widget build(BuildContext context) {
-		//This line grabs the current width of the window. I use it to set the width of the initiative cards to 60% of the width of the application's window
-		double screenWidth = MediaQuery.of(context).size.width;
 
-		return Padding(
-				padding: const EdgeInsets.all(8.0),
-				child: SizedBox(
-					width: screenWidth * 0.6, //Uses the MediaQuery defined above to set the card size to 60% of the window (this is to have room for the Status Effects panel without having to do something disgusting like dynamic resizing of the cards based on status effects panel state).
-					child: Card(
-						elevation: 3,
-							child: Container(
-									padding: const EdgeInsets.all(8),
-									child: Row(
-										children: [
-											Column(
-												children: const [
-													Text("Initiative"),
-													CircleAvatar(
-														// backgroundColor: Colors.amber,
-														child: Text("18"),
-													)
-												],
-											),
-											Expanded(
-												child: Column(children: const [
-													Align(
-														alignment: Alignment.center,
-														child: Text("John Doe"),
-													),
-													//TODO: Do the health bar (Which is probably going to Suck to do).
-													Text("HEALTH BAR GOES HERE")
-												]),
-											),
-											// Column(
-											//   children: [
-											//     const Text("Abilities"),
-											//     IconButton(
-											//         onPressed: showAbilitiesPanel,
-											//         icon: const Icon(Icons.workspaces_outline))
-											//   ],
-											// ),
-											const VerticalDivider(),
+  /// build()
+  /// Parameters: BuildContext context
+  /// Returns: Widget
+  /// Description: Method to construct the InitiativeCard Widget. Instances of this widget are responsible for displaying and managing the state of the initiative blocks
+  /// Special: Method overrides Widget.build()
+  @override
+  Widget build(BuildContext context) {
+    //This line grabs the current width of the window. I use it to set the width of the initiative cards to 60% of the width of the application's window
+    double screenWidth = MediaQuery.of(context).size.width;
 
-											Column(
-												children: [
-													const Text(
-															"Effects"), //Should consider renaming this to "Status Effects" for clarity
-													IconButton(
-															onPressed: _showEffectsPanel,
-															icon: (_shouldDisplayConditionsCard
-                                ? const Icon(Icons.star_rounded)
-                                : const Icon(Icons.star_border_rounded)
-                              ))
-												],
-											)
-										],
-									))),
-				));
-	}
+    return Row(
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: screenWidth *
+                  0.6, //Uses the MediaQuery defined above to set the card size to 60% of the window (this is to have room for the Status Effects panel without having to do something disgusting like dynamic resizing of the cards based on status effects panel state).
+              child: Card(
+                  elevation: 3,
+                  child: Container(
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: const [
+                              Text("Initiative"),
+                              CircleAvatar(
+                                // backgroundColor: Colors.amber,
+                                child: Text("18"),
+                              )
+                            ],
+                          ),
+                          Expanded(
+                            child: Column(children: const [
+                              Align(
+                                alignment: Alignment.center,
+                                child: Text("John Doe"),
+                              ),
+                              //TODO: Do the health bar (Which is probably going to Suck to do).
+                              Text("HEALTH BAR GOES HERE")
+                            ]),
+                          ),
+                          // Column(
+                          //   children: [
+                          //     const Text("Abilities"),
+                          //     IconButton(
+                          //         onPressed: _showAbilitiesPanel,
+                          //         icon: const Icon(Icons.workspaces_outline))
+                          //   ],
+                          // ),
+                          const VerticalDivider(),
 
-	/// showEffectsPanel()
-	/// Parameters: 
-	/// Returns: N/A (void)
-	/// Description: Method responsible for showing and hiding the Status Effects flyout.
-	void _showEffectsPanel() {
-		//TODO: Consider adding code to this method to highlight the button in some way when the panel is open.
+                          Column(
+                            children: [
+                              const Text(
+                                  "Effects"), //Should consider renaming this to "Status Effects" for clarity
+                              IconButton(
+                                  onPressed: _showEffectsPanel,
+                                  icon: (_shouldDisplayConditionsCard
+                                      ? const Icon(Icons.star_rounded)
+                                      : const Icon(Icons.star_border_rounded)))
+                            ],
+                          )
+                        ],
+                      ))),
+            )),
+        Visibility.maintain(
+          visible: _shouldDisplayConditionsCard,
+          child: Container(
+            width: 200,
+            height: 300,
+            margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Card(
+              child: Column(
+                children: [
+                  Text("This is the Conditions Card lol")
+                ],
+              )
+            ),
+            ),
+        )
+      ],
+    );
+  }
+
+  /// showEffectsPanel()
+  /// Parameters:
+  /// Returns: N/A (void)
+  /// Description: Method responsible for showing and hiding the Status Effects flyout.
+  void _showEffectsPanel() {
+    //TODO: Consider adding code to this method to highlight the button in some way when the panel is open.
 
     setState(() {
       if (_shouldDisplayConditionsCard) {
@@ -95,40 +115,39 @@ class _InitiativeCardState extends State<InitiativeCard> {
       }
     });
 
-		// // InitativeCard will need to be converted to a stateful widget in order to get the icon to change state *fairly* automatically.
-		// log("Should show status effects panel");
-	}
+    // // InitativeCard will need to be converted to a stateful widget in order to get the icon to change state *fairly* automatically.
+    // log("Should show status effects panel");
+  }
 
-	/// showAbilitiesPanel()
-	/// Parameters: 
-	/// Returns: N/A (void)
-	/// Description: Method responsible for showing and hiding the Abilities drop-down/flyout.
-	void showAbilitiesPanel() {
-		//TODO: Consider adding code to this method to highlight the button in some way when the panel is open.
-		
-		// InitativeCard will need to be converted to a stateful widget in order to get the icon to change state *fairly* automatically.
-		log("Should show Abilities panel");
-	}
+  /// showAbilitiesPanel()
+  /// Parameters:
+  /// Returns: N/A (void)
+  /// Description: Method responsible for showing and hiding the Abilities drop-down/flyout.
+  void _showAbilitiesPanel() {
+    //TODO: Consider adding code to this method to highlight the button in some way when the panel is open.
+
+    // InitativeCard will need to be converted to a stateful widget in order to get the icon to change state *fairly* automatically.
+    log("Should show Abilities panel");
+  }
 }
 
 class InitiativeCardContainer extends StatelessWidget {
-	// This declaration makes any parameters needed available to instances of the class. The Java equivalent is a constructor method
-	const InitiativeCardContainer({
-		Key? key,
-	}) : super(key: key);
+  // This declaration makes any parameters needed available to instances of the class. The Java equivalent is a constructor method
+  const InitiativeCardContainer({
+    Key? key,
+  }) : super(key: key);
 
-	/// build()
-	/// Parameters: BuildContext context
-	/// Returns: Widget
-	/// Description: Method to construct the UI around the InitiativeCard Widget. This widget may remain stateless unless Flutter takes issue with it.
-	/// Special: Method overrides Widget.build()
-	@override
-	Widget build(BuildContext context) {
-		return Container(
-			alignment: Alignment.centerLeft,
-			margin: const EdgeInsets.all(10),
-			// width: 90,
-			child: const InitiativeCard(),
-		);
-	}
+  /// build()
+  /// Parameters: BuildContext context
+  /// Returns: Widget
+  /// Description: Method to construct the UI around the InitiativeCard Widget. This widget may remain stateless unless Flutter takes issue with it.
+  /// Special: Method overrides Widget.build()
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: const EdgeInsets.all(10),
+      child: const InitiativeCard(),
+    );
+  }
 }
