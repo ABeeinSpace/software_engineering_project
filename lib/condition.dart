349dc80b120a2d5b16a64dafbd70ecfb6eea8c32
ABeeinSpace <planes4u@outlook.com>
2023-03-03 15:10:23 -0500

    Adding Condition object

---
+++ lib/condition.dart
@@ -0,0 +1,32 @@
+class Condition implements Comparable<Condition> {
+  String name = "";
+  int duration = 0;
+  int elapsedTime = 0;
+  
+  Condition({
+    required String name,
+    required int duration,
+    required int elapsedTime
+    });
+  
+  
+  get getName => name;
+
+  set setName( name) => this.name = name;
+
+  get getDuration => duration;
+
+  set setDuration( duration) => this.duration = duration;
+
+  get getElapedTime => elapsedTime;
+
+  set setElapedTime( elapedTime) => this.elapsedTime = elapedTime;
+
+  
+  @override
+  int compareTo(Condition other) {
+    throw UnimplementedError();
+  }
+
+}
+
