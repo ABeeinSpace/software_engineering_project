a282d0c23c573b0cb90ebfcb842e691bee7e65a5
RoweYourBoat <rowejoshua88@gmail.com>
2023-03-19 13:26:48 -0400

    Rmvd xtra init buttn, cleaned up comments

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp(
-      {super.key}); // This declaration makes any parameters needed available to instances of the class. The Java equivalent is a constructor method.
-
-  // This widget is the root of your application.
+  const MyApp({super.key});
 
   /// build()
   /// Parameters: BuildContext context
@@ -23,15 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Combat Scribe',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         useMaterial3: true,
         primarySwatch: Colors.purple,
       ),
@@ -41,19 +29,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  // This declaration makes any parameters needed available to instances of the class. The Java equivalent is a constructor method.
-  const MyHomePage(
-      {super.key,
-      required this.title}); //The required keyword is the same as making a named parameter in Java. It tells the Dart compiler to emit an error if we attempt to instantiate an instance of the class without a parameter.
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -68,17 +44,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int numOfThings = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //
-  //   });
-  // }
 
   late TextEditingController controller;
 
@@ -120,12 +85,6 @@ class _MyHomePageState extends State<MyHomePage> {
   /// Special: Method overrides Widget.build()
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       // Start of Header
       appBar: AppBar(
@@ -148,26 +107,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           ButtonBar(
             children: [
-              IconButton(
-
-                  ///When the add initative button is pressed, open a dialog for the user to input their name
-                  onPressed: () async {
-                    final name = await openDialog();
-                    if (name == null || name.isEmpty) return;
-                    setState(() => this.name = name);
-
-                    //When the user has submitted a name, open a dialog for them to input the Initiative
-                    final initiative = await openInitiativeDialog();
-                    if (initiative == null || initiative.isEmpty) return;
-                    setState(() => this.initiative = initiative);
-
-                    editInitiativeCard();
-                    if (numOfThings == 1) {
-                      addElevation(currentIndex, arr[currentIndex]);
-                    }
-                  },
-                  tooltip: "Add Initiative",
-                  icon: const Icon(Icons.add)),
               // This button will display a drop-down to enable addition of prefab monsters in addition to custom initiatives.
               IconButton(
                   onPressed: _settingsButtonPressed,
@@ -181,35 +120,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
       // Start of Body
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: SingleChildScrollView(
           child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Invoke "debug painting" (press "p" in the console, choose the
-              // "Toggle Debug Paint" action from the Flutter Inspector in Android
-              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-              // to see the wireframe for each widget.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: arr
-              // const Text(
-              //   "You have added this many things:",
-              // ),
-              // Text(
-              //   '$_numOfThings',
-              //   style: Theme.of(context).textTheme.headline4,
-              // ),
-              ),
+              children: arr),
         ),
       ),
 
@@ -218,16 +133,6 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: () async {
           log("Button Pressed!");
           showDialogWithFields();
-          // final name = await openDialog();
-          // if (name == null || name.isEmpty) return;
-          // setState(() => this.name = name);
-
-          // //When the user has submitted a name, open a dialog for them to input the Initiative
-          // final Initiative = await openInitiativeDialog();
-          // if (Initiative == null || Initiative.isEmpty) return;
-          // setState(() => this.Initiative = Initiative);
-
-          // editInitiativeCard();
         },
         icon: const Icon(Icons.add),
         label: const Text("Add Initiative"),
@@ -248,7 +153,6 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text('Enter Player Info'),
           content: SingleChildScrollView(
               child: Column(
-            //shrinkWrap: true,
             children: [
               TextFormField(
                 controller: nameController,
@@ -267,7 +171,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             TextButton(
               onPressed: () {
-                // Send them to your email maybe?
                 var name = nameController.text;
                 var initiative = initiativeController.text;
                 editInitiativeCard2(name, initiative);
@@ -434,7 +337,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void nextButtonPressed() {
     //A setState call to edit the cards with the new elevation
     setState(() => this.elevate = elevate);
-    // 
+    //
 
     //Store the index of the card we are moving away from
     int pastIndex = currentIndex;
@@ -451,13 +354,12 @@ class _MyHomePageState extends State<MyHomePage> {
     //Call the addElevation method with the index of the card we are looking at now and the card itself
     setState(() {
       arr[currentIndex].elevate = 15;
-
     });
-      // addElevation(currentIndex, arr[currentIndex]);
+    // addElevation(currentIndex, arr[currentIndex]);
 
     //Call the removeElevation method with the index of the card we just looked at and the card itself
-      arr[pastIndex].elevate = 3;
-      // removeElevation(pastIndex, arr[pastIndex]);
+    arr[pastIndex].elevate = 3;
+    // removeElevation(pastIndex, arr[pastIndex]);
   }
 
   /// addElevation()
